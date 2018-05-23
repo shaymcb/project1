ec30f12dc2e16a78ad1b24288006f7a899a0e2b1
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-05-23 08:39:56 -0400

    Update othello.py

--- othello.py
+++ othello.py
@@ -1,1 +1,16 @@
+#Shaylee McBride
+#23May2018
+#othello.py
 
+from ggame import *
+
+def buildBoard:
+    
+    for i in range(8):
+        for j in range(8):
+            Sprite(box)
+
+
+
+if __name__ == '__main__':
+    
