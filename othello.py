442dd44f5fedfe802233bba5897e9c47adde8381
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-05-24 08:50:10 -0400

    Updated from Brython Server: 5/24/2018 8:50:09 AM

--- othello.py
+++ othello.py
@@ -13,26 +13,43 @@ def buildBoard():
         for cols in range(8):
             Sprite(box,(cols*BOX_SIZE,rows*BOX_SIZE))
             L.append('')
-        data['pieceList'].append('')
+        pieceList.append(L)
+     
+    spriteList.append(Sprite(blackCircle,(3*BOX_SIZE,4*BOX_SIZE)))
+    spriteList.append(Sprite(blackCircle,(4*BOX_SIZE,3*BOX_SIZE)))    
+    spriteList.append(Sprite(whiteCircle,(4*BOX_SIZE,4*BOX_SIZE)))
+    spriteList.append(Sprite(whiteCircle,(3*BOX_SIZE,3*BOX_SIZE)))
     
-    data['pieceList'][4][3] = Sprite(blackCircle,(3*BOX_SIZE,4*BOX_SIZE))
-    data['pieceList'][3][4] = Sprite(blackCircle,(4*BOX_SIZE,3*BOX_SIZE))
-    data['pieceList'][4][4] = Sprite(whiteCircle,(4*BOX_SIZE,4*BOX_SIZE))
-    data['pieceList'][3][3] = Sprite(whiteCircle,(3*BOX_SIZE,3*BOX_SIZE))
+    pieceList[4][3] = 'b'
+    pieceList[3][4] = 'b'
+    pieceList[4][4] = 'w'
+    pieceList[3][3] = 'w'
+    
+def mouseClick(event):
+    clickCol = event.x//BOX_SIZE
+    clickRow = event.y//BOX_SIZE
+    color = 'b'
+    
+    sameRow = False
+    sameCol = False
+    sameDiag = False
+    if 
 
 
 if __name__ == '__main__':
-    data = {}
-    data['pieceList'] = []
+    pieceList = []
+    spriteList = []
     green = Color(0x00FF00, 1)
     white = Color(0xFFFFFF, 1)
     black = Color(0x000000, 1)
     outline = LineStyle(1,black)
     
     box = RectangleAsset(BOX_SIZE,BOX_SIZE,outline,green)
-    blackCircle  = CircleAsset(BOX_SIZE,outline,black)
-    whiteCircle = CircleAsset(BOX_SIZE,outline,white)
+    blackCircle  = CircleAsset(BOX_SIZE/2,outline,black)
+    whiteCircle = CircleAsset(BOX_SIZE/2,outline,white)
     
     buildBoard()
+    
+    App.listenMouseEvent('click',mouseClick)
     
     App().run()
