0ce079a9bc4f83afd792c20eb46c7f1ed3afd0e2
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-06-05 08:07:07 -0400

    Update bullsAndCows.py

--- bullsAndCows.py
+++ bullsAndCows.py
@@ -1,204 +1,228 @@
 #Shaylee McBride
-#4Apr2018
-#bullsAndCows.py - mastermind
+#23May2018
+#othello.py
+
+#add a sign for whose turn it is
+#add an endgame
+#add comments
+#"winner" function
+#what is point of "flipPieces" function
 
 from ggame import *
-from random import randint
 
-#constants
-RADIUS = 20   #how big the circles are
-ATTEMPTS = 10 #how many tries you get
+BOX_SIZE = 70
 
-#colors
-red = Color(0xFF0000,1)
-orange = Color(0xFFA500,1)
-yellow = Color(0xFFFF00,1)
-green = Color(0x00FF00,1)
-cyan = Color(0x00FFFF,1)
-blue = Color(0x0000FF,1)
-violet = Color(0x800080,1)
-pink = Color(0xFF00FF,1)
-white = Color(0xFFFFFF,1)
-black = Color(0x000000,1)
-grey = Color(0xF4F4F4,1)
-outline = LineStyle(2,black)
-
-#returns the code to be guessed
-def pickCode():
-    #generates 4 random digits
-    pick1 = randint(0,9)
-    pick2 = randint(0,9)
-    pick3 = randint(0,9)
-    pick4 = randint(0,9)
+#displays board, score, and current pieces in list
+def buildBoard():
+    #board
+    for row in range(8):
+        L = []
+        for col in range(8):
+            Sprite(box,(col*BOX_SIZE,row*BOX_SIZE))
+            if pieceList[row][col] == 1:
+                Sprite(blackCircle,(col*BOX_SIZE,row*BOX_SIZE))
+            elif pieceList[row][col] == 2:  
+                Sprite(whiteCircle,(col*BOX_SIZE,row*BOX_SIZE))
     
-    #if any of the four overlap, pick again until they don't
-    while pick2 == pick1:
-        pick2 = randint(0,9)
-    while pick3 == pick1 or pick3 == pick2:
-        pick3 = randint(0,9)
-    while pick4 == pick1 or pick4 == pick2 or pick4 == pick3:
-        pick4 = randint(0,9)
-    
-    #store it as a string because it's easier to work with
-    data['code1'] = str(pick1)
-    data['code2'] = str(pick2)
-    data['code3'] = str(pick3)
-    data['code4'] = str(pick4)
-    
-    return data['code1'] + data['code2'] + data['code3'] + data['code4']
-
-#sprites a circle of the guessed color in the correct spot
-def enterColor(color):
-    Circle = CircleAsset(20,outline,color)
-    if data['guesses'] < 4 * ATTEMPTS: #only goes for number of attempts possible
-        Sprite(Circle,((data['guesses'] - data['guesses']//4 * 4)*45, data['guesses']//4 * 45))
-        data['guesses'] += 1
-
-#all of these do the same thing but with different colors:
-#sets the color for turnColor() and checks answer if 4 have been guessed
-def turnRed(event):
-    if '0' not in data['guess']: #can't guess same color twice
-        enterColor(red)
-        data['guess'] += '0'
-        if data['guesses']%4 == 0:
-                checkCode(data['guess'])
-def turnOrange(event):
-    if '1' not in data['guess']:
-        enterColor(orange)
-        data['guess'] += '1'
-        if data['guesses']%4 == 0:
-                checkCode(data['guess'])
-def turnYellow(event):
-    if '2' not in data['guess']:
-        enterColor(yellow)
-        data['guess'] += '2'
-        if data['guesses']%4 == 0:
-                checkCode(data['guess'])
-def turnGreen(event):    
-    if '3' not in data['guess']:
-        enterColor(green)
-        data['guess'] += '3'
-        if data['guesses']%4 == 0:
-                checkCode(data['guess'])
-def turnCyan(event):    
-    if '4' not in data['guess']:
-        enterColor(cyan)
-        data['guess'] += '4'
-        if data['guesses']%4 == 0:
-                checkCode(data['guess'])
-def turnBlue(event):    
-    if '5' not in data['guess']:
-        enterColor(blue)
-        data['guess'] += '5'
-        if data['guesses']%4 == 0:
-                checkCode(data['guess'])
-def turnPink(event):
-    if '6' not in data['guess']:
-        enterColor(pink)
-        data['guess'] += '6'
-        if data['guesses']%4 == 0:
-                checkCode(data['guess'])
-def turnViolet(event):    
-    if '7' not in data['guess']:
-        enterColor(violet)
-        data['guess'] += '7'
-        if data['guesses']%4 == 0:
-                checkCode(data['guess'])
-def turnWhite(event):    
-    if '8' not in data['guess']:
-        enterColor(white)
-        data['guess'] += '8'
-        if data['guesses']%4 == 0:
-                checkCode(data['guess'])
-def turnDark(event):    
-    if '9' not in data['guess']:
-        enterColor(black)
-        data['guess'] += '9'
-        if data['guesses']%4 == 0:
-                checkCode(data['guess'])
-
-#checks 4-digit guess against answer code, displays bulls and cows
-def checkCode(answer):
-    if checkBulls(data['guess']) == 4: #if you got it right, displays win message in correct spot
-        Sprite(winBox,(RADIUS*8 + 25, (data['guesses']/4-1)*(RADIUS*2+5)))
-    elif data['guesses'] == ATTEMPTS * 4: #if you're out of guesses, displays lose message in correct spot
-        Sprite(loseBox, (RADIUS*8 + 25, (ATTEMPTS-1)*(RADIUS*2+5)))
+    #pieces            
+    Sprite(TextAsset('Turn:'),(BOX_SIZE*8.2,BOX_SIZE*.4))
+    if data['player'] == 1:
+        Sprite(blackCircle,(BOX_SIZE*9,BOX_SIZE*.1))
     else:
-        bulls = checkBulls(data['guess'])
-        cows = checkCows(data['guess'])
-        scoreBox = TextAsset('Bulls = '+str(bulls)+' , Cows = '+str(cows))
-        Sprite(scoreBox,(RADIUS*8 + 25, (data['guesses']/4-1)*(RADIUS*2+5)))
+        Sprite(whiteCircle,(BOX_SIZE*9,BOX_SIZE*.1))
     
-    data['guess'] = '' #reset guess and try again
-
-#checks how many guessed colors are right color in right spot
-#for each digit in the guess, checks against corresponding digit in code
-def checkBulls(answer):
-    bulls = 0
-    i = 0
-    for ch in data['guess']: 
-        i += 1
-        if i == 1:
-            if ch == data['code1']:
-                bulls += 1
-        elif i == 2:
-            if ch == data['code2']:
-                bulls+=1
-        elif i == 3:
-            if ch == data['code3']:
-                bulls+=1
-        else:
-            if ch == data['code4']:
-                bulls+=1
-    return bulls
-
-#checks how many guessed colors are right, but in wrong spot
-#counts colors in both guess and code and subtracts ones in right spot
-def checkCows(answer):
-    cows = 0
-    match = '' 
-    for ch in data['guess']:
-        if ch in data['code']:
-            cows += 1
-    return cows - checkBulls(data['guess'])
+    #score
+    Sprite(TextAsset('Black: '+str(scoreList[0])+' White: '+str(scoreList[1])),(BOX_SIZE*8.2,BOX_SIZE*1.5))  
     
+    #detects where mouse clicks, decides if it's legal, and calls flips
+def mouseClick(event):
+    #converts mouse x and y into columns and rows
+    clickCol = int(event.x//BOX_SIZE)
+    clickRow = int(event.y//BOX_SIZE)
+    
+    if clickRow > 7 or clickCol > 7 or pieceList[clickRow][clickCol] != '': #breaks if illegal move
+        return False
+
+    flipped = flipPieces(clickRow, clickCol)
+    
+    #if any of the pieces flipped, it's a legal move so update board and switch players
+    if flipped == True:
+        pieceList[clickRow][clickCol] = data['player']
+        data['player'] = 3 - data['player']
+        data['otherPlayer'] = 3 - data['otherPlayer']
+        updateScore()
+        redrawAll()
+        winner()
+
+def flipPieces(row,col):
+    #calls functions to flip pieces
+    w = flipWest(row,col)
+    e = flipEast(row,col)
+    n = flipNorth(row,col)
+    s = flipSouth(row,col)
+    nw = flipNorthWest(row,col)
+    se = flipSouthEast(row,col)
+    ne = flipNorthEast(row,col)
+    sw = flipSouthWest(row,col)
+    
+    if w == True or e == True or n == True or s == True or nw == True or se == True or ne == True or sw == True:
+        return True
+  
+def flipWest(row,col):
+    status = False
+    for i in range(col-1,-1,-1):
+        if pieceList[row][i] == '':
+            break
+        elif pieceList[row][i] == data['player']:
+            for j in range(i,col):
+                if pieceList[row][j] == data['otherPlayer']: #if other color
+                    pieceList[row][j] = data['player'] #technically this doesn't need to be in this 'if' bc it's already determined that only the other color lies in this range
+                    status = True #but I needed the status part to get rid of some illegal moves
+            break
+    return status
+
+def flipEast(row,col):
+    status = False
+    for i in range(col+1,8):  #from the column after the clicked one to the end
+        if pieceList[row][i] == '': #stop if encounter an empty cell
+            break
+        elif pieceList[row][i] == data['player']:  #if find another of same color
+            for j in range(col+1,i):  #check between them
+                if pieceList[row][j] == data['otherPlayer']:
+                    pieceList[row][j] = data['player']  #flip
+                    status = True
+            break
+    return status
+
+def flipNorth(row,col):
+    status = False
+    for i in range(row-1,-1,-1):
+        if pieceList[i][col] == '':
+            break
+        elif pieceList[i][col] == data['player']:  #if this row has a matching piece above in same column
+            for j in range(i, row): #check between them for other color
+                if pieceList[j][col] == data['otherPlayer']:
+                    pieceList[j][col] = data['player']
+                    status = True
+            break
+    return status
+
+
+def flipSouth(row,col):
+    status = False
+    for i in range(row+1,8):
+        if pieceList[i][col] == '':
+            break
+        elif pieceList[i][col] == data['player']:  #if this row has a matching piece below in same column
+            for j in range(row+1,i): #check between them for other color
+                if pieceList[j][col] == data['otherPlayer']:
+                    pieceList[j][col] = data['player']
+                    status = True
+            break
+    return status
+            
+
+def flipNorthWest(row,col):
+    status = False
+    for i in range(1,min(col,row)+1):
+        if pieceList[row-i][col-i] == '':
+            break
+        elif pieceList[row-i][col-i] == data['player']:
+            for j in range(1,i):
+                if pieceList[row-j][col-j] == data['otherPlayer']:
+                    pieceList[row-j][col-j] = data['player']
+                    status = True
+            break
+    return status
+
+def flipSouthEast(row,col):
+    status = False
+    for i in range(1,min(8-col,8-row)):
+        if pieceList[row+i][col+i] == '':
+            break
+        elif pieceList[row+i][col+i] == data['player']:
+            for j in range(1,i):
+                if pieceList[row+j][col+j] == data['otherPlayer']:
+                    pieceList[row+j][col+j] = data['player']
+                    status = True
+            break
+    return status
+            
+
+
+def flipSouthWest(row,col):
+    status = False
+    for i in range(1,min(col,7-row)):
+        if pieceList[row+i][col-i] == '':
+            break
+        elif pieceList[row+i][col-i] == data['player']:
+            for j in range(1,i):
+                if pieceList[row+j][col-j] == data['otherPlayer']:
+                    pieceList[row+j][col-j] = data['player']
+                    status = True
+            break
+    return status
+       
+       
+def flipNorthEast(row,col):
+    status = False
+    for i in range(1,min(8-col,row)):
+        if pieceList[row-i][col+i] == '':
+            break
+        elif pieceList[row-i][col+i] == data['player']:
+            for j in range(1,i):
+                if pieceList[row-j][col+j] == data['otherPlayer']:
+                    pieceList[row-j][col+j] = data['player']
+                    status = True
+            break
+    return status
+    
+def updateScore():
+    black = 0
+    white = 0
+    for row in pieceList:
+        black += row.count(1)
+        white += row.count(2)
+    scoreList[0] = black
+    scoreList[1] = white
+
+def redrawAll():
+    for item in App().spritelist[:]:
+        item.destroy()
+    
+    buildBoard()
+
+def winner():
+    for row in pieceList:
+        for col in row:
+            if col == '':
+                break
+            elif row == 7:
+                if scoreList[0] > scoreList[1]:
+                    Sprite(TextAsset("Black Wins!",style='bold 40pt Times'),(BOX_SIZE*9,BOX_SIZE*.7))
+                elif scoreList[0] < scoreList[1]:
+                    Sprite(TextAsset("White Wins!",style='bold 40pt Times'),(BOX_SIZE*9,BOX_SIZE*.7))
+                else:
+                    Sprite(TextAsset("Tie Game",style='bold 40pt Times'),(BOX_SIZE*9,BOX_SIZE*.7))
+
 if __name__ == '__main__':
+    pieceList = [['', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', ''], ['', '', '', 2, 1, '', '', ''], ['', '', '', 1, 2, '', '', ''], ['', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '']]
+    scoreList = [2,2]
     data = {}
-    data['code1'] = ''
-    data['code2'] = ''
-    data['code3'] = ''
-    data['code4'] = ''
-    data['code'] = pickCode()
-    data['guesses'] = 0  #total number of guessed colors
-    data['guess'] = ""   #guessed code
+    data['player'] = 1
+    data['otherPlayer'] = 2
     
-    emptyCircle = CircleAsset(RADIUS,outline,grey)
-    key = TextAsset('Possible Colors: r, o, y, g, c, b, p, v, w, d',width = 500)
-    scoreBox = TextAsset('Bulls = 0, Cows = 0')
-    winBox = TextAsset('YOU WIN!!!',style = 'bold 40pt Times')
-    loseBox = TextAsset('YOU LOSE :(',style = 'bold 40pt Times',width = 200)
-
-    #guessing grid
-    circlex = 0
-    circley = 0
-    for i in range(ATTEMPTS):
-        for j in range(4):
-            Sprite(emptyCircle,(circlex + j*(2*RADIUS+5),circley + i*(2*RADIUS+5)))
+    green = Color(0x00FF00, 1)
+    white = Color(0xFFFFFF, 1)
+    black = Color(0x000000, 1)
+    outline = LineStyle(1,black)
     
-    Sprite(key,(300,0))
+    box = RectangleAsset(BOX_SIZE,BOX_SIZE,outline,green)
+    blackCircle  = CircleAsset(BOX_SIZE/2,outline,black)
+    whiteCircle = CircleAsset(BOX_SIZE/2,outline,white)
     
-    App().listenKeyEvent('keydown','r',turnRed)
-    App().listenKeyEvent('keydown','o',turnOrange)
-    App().listenKeyEvent('keydown','y',turnYellow)
-    App().listenKeyEvent('keydown','g',turnGreen)
-    App().listenKeyEvent('keydown','c',turnCyan)
-    App().listenKeyEvent('keydown','b',turnBlue)
-    App().listenKeyEvent('keydown','p',turnPink)
-    App().listenKeyEvent('keydown','v',turnViolet)
-    App().listenKeyEvent('keydown','w',turnWhite)
-    App().listenKeyEvent('keydown','d',turnDark)
+    buildBoard()
+    
+    App.listenMouseEvent('click',mouseClick)
     
     App().run()
-    
-    
-    
